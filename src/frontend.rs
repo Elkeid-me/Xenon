8870f173b35af06e6bb0b1342f4761acbc803783
Elkeid-me <elkeid.me@gmail.com>
2024-01-24 22:04:22 +0800

    引入 pest 依赖

--- src/frontend.rs
+++ src/frontend.rs
@@ -1,4 +1,11 @@
+use pest::Parser;
+use pest_derive::Parser;
+
 mod ast;
+
+#[derive(Parser)]
+#[grammar = "frontend/sysy.pest"]
+struct SYSYParser;
 
 pub fn generate_ir(code: &String) -> koopa::ir::Program {
     todo!()
