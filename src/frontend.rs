4ca08d38a1a854a6955ee6e50987e65ba92c284e
Elkeid-me <elkeid.me@gmail.com>
2024-01-30 21:54:12 +0800

    测试，拿来玩的

--- src/frontend.rs
+++ src/frontend.rs
@@ -4,6 +4,10 @@ mod parser;
 
 pub struct Frontend {}
 
+pub fn generate_ast(code: &str) -> ast::TranslationUnit {
+    parser::AstBuilder::new().build_ast(code)
+}
+
 pub fn generate_ir(code: &str) -> koopa::ir::Program {
     todo!()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::{fs::read_to_string, io::Write};
+
 mod arg_parse;
 mod frontend;
 mod preprocessor;
@@ -5,4 +7,13 @@ mod preprocessor;
 fn compile() {
     todo!()
 }
-fn main() {}
+fn main() {
+    let mut fs = std::fs::File::create("test/result.txt").unwrap();
+    write!(
+        &mut fs,
+        "{:#?}",
+        frontend::generate_ast(&preprocessor::preprocess(
+            &std::fs::read_to_string("test/src.txt").unwrap()
+        ))
+    );
+}
