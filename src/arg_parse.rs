9a8e143eed5431bc7394fac97fd424d9fca32883
Elkeid-me <elkeid.me@gmail.com>
2024-02-10 16:14:20 +0800

    实验性的参数解析

--- src/arg_parse.rs
+++ src/arg_parse.rs
@@ -1,4 +1,4 @@
-use std::{env, error};
+use std::env::Args;
 
 pub enum Mode {
     Koopa,
@@ -6,6 +6,17 @@ pub enum Mode {
     Optimization,
 }
 
-pub fn parse(args: env::Args) {
-    todo!()
+pub type ParsedArgs = (Mode, String, String);
+
+pub fn parse(mut args: Args) -> Result<ParsedArgs, String> {
+    let args = &mut args;
+    let mode = match args.skip(1).next().unwrap().as_str() {
+        "-koopa" => Ok(Mode::Koopa),
+        "-riscv" => Ok(Mode::RiscV),
+        "-perf" => Ok(Mode::Optimization),
+        s => Err(format!("未知的模式: {}", s)),
+    }?;
+    let input = args.next().unwrap();
+    let output = args.skip(1).next().unwrap();
+    Ok((mode, input, output))
 }
